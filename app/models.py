bc496f2c18159e2a5b8e3ba9250f9ba98c693677
Tactful-biao <1208339113@qq.com>
2021-06-30 13:56:55 +0800

    通过函数的方法更新数据库, 添加自关注

--- app/models.py
+++ app/models.py
@@ -110,7 +110,7 @@ class User(UserMixin, db.Model):
 
     if self.email is not None and self.avatar_hash is None:
       self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
-
+    self.follow(self)
 
   def can(self, permissions):
     return self.role is not None and (self.role.permissions & permissions) == permissions
@@ -243,6 +243,14 @@ class User(UserMixin, db.Model):
   @property
   def followed_posts(self):
     return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
+
+  @staticmethod
+  def add_self_follows():
+    for user in User.query.all():
+      if not user.is_following(user):
+        user.follow(user)
+        db.session.add(user)
+        db.session.commit()
 
   def __repr__(self):
     return '<Users %r>' % self.username
