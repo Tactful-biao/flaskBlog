7e4c06f0367d1ffba6141a731dfdc8c0012dbdd8
Tactful-biao <1208339113@qq.com>
2021-06-30 13:25:45 +0800

    添加关注文章筛选

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, abort, flash, request, current_app
+from flask import render_template, session, redirect, url_for, abort, flash, request, current_app, make_response
 from datetime import datetime
 
 from . import main
@@ -16,8 +16,15 @@ def index():
     post = Post(body=form.body.data, author=current_user._get_current_object())
     db.session.add(post)
     return redirect(url_for('.index'))
+  show_followed = False
+  if current_user.is_authenticated:
+    show_followed = bool(request.cookies.get('show_followed', ''))
+  if show_followed:
+    query = current_user.followed_posts
+  else:
+    query = Post.query
   page = request.args.get('page', 1, type=int)
-  pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page,
+  pagination = query.order_by(Post.timestamp.desc()).paginate(page,
                per_page=current_app.config['FLASK_POSTS_PER_PAGE'], error_out=False)
   posts = pagination.items
   return render_template("index.html", form=form, posts=posts, pagination=pagination)
@@ -157,3 +164,19 @@ def followed_by(username):
   pagination = user.followed.paginate(page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'], error_out=False)
   follows = [{'user': item.followed, 'timestamp': item.timestamp} for item in pagination.items]
   return render_template('followers.html', user=user, title='Followed by', endpoint='.followed_by', pagination=pagination, follows=follows)
+
+
+@main.route('/all')
+@login_required
+def show_all():
+  resp = make_response(redirect(url_for('.index')))
+  resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+  return resp
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+  resp = make_response(redirect(url_for('.index')))
+  resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+  return resp
